08a31bb0d26957ff74649bc01c4855963f608487
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-08-15 22:17:49 +0300

    More Chane

--- app/GeneralModels/CoverRequirements.php
+++ app/GeneralModels/CoverRequirements.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class CoverRequirements extends Model
 {
-    //
+    public $table = "cover_requirements";
+    protected $guarded = ["id"];
+
+    public function CoverRequirementsHasManyCoverQuestions()
+    {
+        return $this->hasMany('App\GeneralModels\CoverQuestion', 'cover_requirement_id', 'id');
+    }
 }
