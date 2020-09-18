ef010ace367e3fea2c10d23e40d4e352235d3b8f
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-09-18 12:36:06 +0300

    Bug Fix

--- app/Http/Controllers/Payments/CustomerToOrganisationController.php
+++ app/Http/Controllers/Payments/CustomerToOrganisationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Payments;
 
 use App\Payments\CustomerToOrganisation;
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\Controller;
 class CustomerToOrganisationController extends Controller
 {
  
