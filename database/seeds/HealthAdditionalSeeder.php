12b86d6d1ffa86ee225608e62a4129db4255d491
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-11-16 11:50:16 +0300

    Added More Covers.

--- database/seeds/HealthAdditionalSeeder.php
+++ database/seeds/HealthAdditionalSeeder.php
@@ -23,6 +23,23 @@ class HealthAdditionalSeeder extends Seeder
                 'name'=> 'MATERNITY COVER OPTION ',
                 'type_of_calculation'=> 0,
              ],
+             [
+                'insurance_covers_id'=> 2,
+                'name'=> 'OUTPATIENT COVER',
+                'type_of_calculation'=> 1,
+             ],
+             [
+              'insurance_covers_id'=> 2,
+              'name'=> 'MATERNITY COVER OPTION ',
+              'type_of_calculation'=> 0,
+           ],
+
+
+             [
+                'insurance_covers_id'=> 1,
+                'name'=> 'MATERNITY COVER OPTION 2',
+                'type_of_calculation'=> 0,
+             ],
             
         ];
         foreach($additional as $key => $value){
--- database/seeds/HealthAdditionlPremiumSeeder.php
+++ database/seeds/HealthAdditionlPremiumSeeder.php
@@ -39,6 +39,43 @@ class HealthAdditionlPremiumSeeder extends Seeder
                 'limit'=> 20000,
                 'cost'=> 5700,                
             ],
+            [
+                'additional_id'=> 5,
+                'limit'=> 40000,
+                'cost'=> 9500,                
+            ],
+            [
+                'additional_id'=> 5,
+                'limit'=> 20000,
+                'cost'=> 5700,                
+            ],
+
+
+            [
+                'additional_id'=> 3,
+                'limit'=> 50000,
+                'cost'=> 10239,                
+            ],
+            [
+                'additional_id'=> 3,
+                'limit'=> 25000,
+                'cost'=> 8165,                
+            ],
+            [
+                'additional_id'=> 3,
+                'limit'=> 10000,
+                'cost'=> 5053,                
+            ],
+            [
+                'additional_id'=> 4,
+                'limit'=> 40000,
+                'cost'=> 9500,                
+            ],
+            [
+                'additional_id'=> 4,
+                'limit'=> 20000,
+                'cost'=> 5700,                
+            ],
         
     ];
     foreach($premium as $key => $value){
