2ad70276337be353b37c55341b43c58ae2eec764
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-11-29 16:41:15 +0300

    Change

--- app/Http/Controllers/Mpesa/LipaNaMpesaController.php
+++ app/Http/Controllers/Mpesa/LipaNaMpesaController.php
@@ -28,7 +28,7 @@ class LipaNaMpesaController extends Controller
     public function customerMpesaSTKPush(Request $request)
     {
 
-        return $request->personalDetails->firstName;
+        return $request->personalDetails['firstName'];
         $cost = $request->cost;
         $phoneNumberEdited = $request->phoneNumberEdited;
         // $email_address= $request->personalDetails['email_address'];
