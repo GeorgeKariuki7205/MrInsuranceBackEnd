5f29bcacad8599ba7b58e36299ecede8b1cf87af
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-12-01 12:34:04 +0300

    Mail Changes

--- resources/views/mail/confirmationOfPayment.blade.php
+++ resources/views/mail/confirmationOfPayment.blade.php
@@ -1,20 +1,7 @@
 @component('mail::message')
 # Hello, {{$personalDetailsArray['name']}},
 
-We highly appreciiate you purchasing an inurance cover through our platform. 
 
----
-Your Premium Details Are As Shown Below,. 
-***
- 
-
-|         |            |
-| :-------------: |:-------------:
-| Insurance Cover| {{$insuranceCoverDetails['name']}}  |
-| Company:      | {{$insuranceCoverDetails['company']}}      |
-| Cover:      | {{$insuranceCoverDetails['cover'] }}      |
-| Sub Category:      | {{$insuranceCoverDetails['SubCategory']}}      |
-| Cost: | are neat      |    $1 |
 
 @component('mail::button', ['url' => 'https://mrinsurance.georgekprojects.tk/'])
 Link To Btn.
