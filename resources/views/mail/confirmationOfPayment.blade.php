9c6d80814e5fa8a2d33d5c5b8f487d838c656573
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-12-02 09:18:18 +0300

    Added Mial Change

--- resources/views/mail/confirmationOfPayment.blade.php
+++ resources/views/mail/confirmationOfPayment.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
-# Hello, {{$personalDetailsArray['name']}},
+# Hello, {{$personalDetailsArray}},
 
 We highly appreciiate you purchasing an inurance cover through our platform. 
 
