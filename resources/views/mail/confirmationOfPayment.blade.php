c53a7e48bc39cfb2a89db4e65e54ce1e9b018422
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-12-02 10:43:45 +0300

    Added Mial Change

--- resources/views/mail/confirmationOfPayment.blade.php
+++ resources/views/mail/confirmationOfPayment.blade.php
@@ -8,9 +8,17 @@ This email spells out the next steps to complete your transaction.
 Your Premium Details Are As Shown Below: 
 
 ***
-{{$insuranceCoverDetailsName}}
+{{-- {{$insuranceCoverDetailsName}}
 {{$numberOfInsuranceCoverModel}}
-{{$intentionId}}
+{{$intentionId}} --}}
+
+|         |            |
+| :-------------: |:-------------:|
+| Insurance Cover| {{$insuranceCoverDetailsName}}  |
+| Company:      | {{$insuranceCoverDetailsCompany}}      |
+| Cover:      | {{$insuranceCoverDetailsCover }}      |
+| Sub Category:      | {{$insuranceCoverDetailsSubCategory}}      |
+| Cost: | are neat      |
 
 @component('mail::button', ['url' => 'https://mrinsurance.georgekprojects.tk/'])
 Activate Your Account.
