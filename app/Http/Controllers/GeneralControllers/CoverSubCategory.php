8be3705173c434c7cd834c191e36bd34ba07b8b2
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-12-29 12:52:25 +0300

    New Function

--- app/Http/Controllers/GeneralControllers/CoverSubCategory.php
+++ app/Http/Controllers/GeneralControllers/CoverSubCategory.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\GeneralControllers;
 
 use App\GeneralModels\SubCategoryCover;
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\Controller;
 class CoverSubCategory extends Controller
 {
     /**
