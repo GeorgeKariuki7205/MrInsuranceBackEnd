6518f8fce71ba647b71b0ca314b14b6daff26fec
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2021-01-04 13:39:02 +0300

    Change function

--- app/Http/Controllers/GettingPersonalDetails.php
+++ app/Http/Controllers/GettingPersonalDetails.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Client;
+use Illuminate\Support\Facades\Hash;
+use Carbon;
 class GettingPersonalDetails extends Controller
 {
     //
